481567c3b71210bd2a3bd00b5fcb17c366af1dda
unndf <phenanimao@gmail.com>
2014-05-10 04:27:52 -0600

    make cropping optional

--- xml2svg.py
+++ xml2svg.py
@@ -2,11 +2,33 @@ import svgwrite as svg
 import xml
 import sys
  
- 
-def convert(input,output,width,height,scale):
+
+def get_size(input):
+    d = xml.etree.ElementTree.parse(input)
+    r = d.getroot()
+
+    width=0
+    height=0
+    for poly in r[0]:
+        for point in poly.find('Points'):
+            x = int(point.find('X').text)
+            y = int(point.find('Y').text)
+            if x > width:
+                width = x
+            if y > height:
+                height = y
+    
+    return (width,height)
+
+def convert(input,output,scale=1,width=0,height=0,crop=False):
+        if not crop:
+            size = get_size(input)
+            width = size[0]
+            height = size[1]
+
         d = xml.etree.ElementTree.parse(input)
         r = d.getroot()
-       
+        
         scale = int(scale)
         width = int(width)
         height = int(height)
@@ -30,6 +52,7 @@ def convert(input,output,width,height,scale):
         canvas.save()
  
 if __name__ == "__main__":
+        #TODO: Better argparsing
         if (len(sys.argv) != 6):
                 print("usage: python xml2svg.py <input.xml> <output.svg> <INT original_width> <INT original_height> <INT scale>")
                 sys.exit(0)
@@ -39,4 +62,4 @@ if __name__ == "__main__":
                 width = str(sys.argv[3])
                 height = str(sys.argv[4])
                 scale = int(sys.argv[5])
-                convert(input,output,width,height,scale)
+                convert(input,output,width=width,height=height,scale=scale,crop=True)
